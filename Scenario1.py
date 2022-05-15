3324e10fcbcffe9b30e7dedb03f16e2e3e0bb20c
NicholasYerolemou <yerolemou.nicholas@gmail.com>
2022-05-16 01:07:35 +0200

    Scenario 1 complete

--- Scenario1.py
+++ Scenario1.py
@@ -1,110 +1,118 @@
+
+import re
 from sqlalchemy import column
+from torch import initial_seed
 import FourRooms
 from random import randrange
 import numpy as np
+import random
 # https://www.analyticsvidhya.com/blog/2021/04/q-learning-algorithm-with-step-by-step-implementation-using-python/
-"""
-FR = FourRooms('simple', bool=False)
-
-
-pos = (int, int)
-pos = FR.getPosition()  # gets position
-
-
-packagesLeft = FR.getPackagesRemaining()
-
-
-cellType, newPos, packagesLeft, isTerminal = FR.takeAction(FR.UP)
-
-
-isTerminal = FR.isTerminal()  # True if sim is over
-
-FR.newEpoch()  # resets the enviroment to starting positions
-
-
-# index’th epoch.
-# showPath(-1) will show the agent’s path for the last recorded epoch.
-# displays the agent path using the matplotlib GUI
-
-epoch = 0
-FR.showPath(epoch, str='path')
-"""
-"""
-# Action Constants
-FourRooms.UP = 0
-FourRooms.DOWN = 1
-FourRooms.LEFT = 2
-FourRooms.RIGHT = 3
-
-# Grid Cell Type Constants
-FourRooms.EMPTY = 0
-FourRooms.RED = 1
-FourRooms.GREEN = 2
-FourRooms.BLUE = 3
-"""
 
 
 # epoch ends when you reach terminal state
 
 
-gamma = 0.8
-reward = 0
-action = 0
-state = (int, int)
+def setRewards():
+    # sets rewards of walls to -100
+    for x in range(13):
+        for y in range(13):
+            if(y == 0 or y == 12 or x == 0 or x == 12):
+                rewards[x, y] = -100
+            if(y < 7 and x == 6):
+                rewards[x, y] = -100
+            if(y > 5 and x == 7):
+                rewards[x, y] = -100
+            if(x < 12 and y == 6):
+                rewards[x, y] = -100
+
+    rewards[3, 6] = -1
+    rewards[6, 2] = -1
+    rewards[10, 6] = -1
+    rewards[7, 9] = -1
 
 
-def chooseAction(FR):
-    num = randrange(4)
-    num = num - 1
-    if(num == 0):
-        return FR.UP
-    if(num == 1):
-        return FR.DOWN
-    if(num == 2):
-        return FR.LEFT
-    if(num == 3):
-        return FR.RIGHT
+# find the action with the highest reward, returns a num 0-3 representing an action
 
 
 FR = FourRooms.FourRooms('simple', False)
 
 rows = 13
 columns = 13
-
-Q_values = np.zeros((rows, columns, 4))  # (state,(Q value for all 4 actions))
+# (state,(Q value for all 4 actions))(state size, action size)
+Q_values = np.zeros((rows*columns, 4))
 actions = [FR.UP, FR.DOWN, FR.LEFT, FR.RIGHT]
 
+ac = ["up", 'down', 'left', 'right']
 # initialises the rewards for every block to -1
 rewards = np.full((rows, columns), -1)
-# sets rewards of walls to -100
-
-for x in range(13):
-    for y in range(13):
-        if(y == 0 or y == 12 or x == 0 or x == 12):
-            rewards[x, y] = -100
-        if(y < 7 and x == 6):
-            rewards[x, y] = -100
-        if(y > 5 and x == 7):
-            rewards[x, y] = -100
-        if(x < 12 and y == 6):
-            rewards[x, y] = -100
-
-rewards[3, 6] = -1
-rewards[6, 2] = -1
-rewards[10, 6] = -1
-rewards[7, 9] = -1
+setRewards()  # sets the reward values
 print(rewards)
 
-FR.newEpoch
-epoch = 1
-print("Epoch", epoch)
-print("Packages left", FR.getPackagesRemaining())
-print("Agent initial position", FR.getPosition())
+epsilon = 0.2  # makes a random choice 20% of the time
+
+# Q value = adding the maximum reward attainable from future states to the reward for achieving its current state
+
+
+def convert(pos):
+    return pos[0] + 13*pos[1]
+
+
+def chooseAction(FR):
+    # print("Choose functionQ values for state", FR.getPosition(),
+    # ":", Q_values[convert(FR.getPosition())])
+    if random.uniform(0, 1) < epsilon:  # random action
+        return random.randint(0, 3)
+
+    else:  # highest Q action
+        return np.argmax(Q_values[convert(FR.getPosition())])
+
+
+# initialise Q values
+discount_factor = 0.8
+learning_rate = 0.8
+
+episodes = 100
+numMoves = 0
 counter = 0
-while(FR.isTerminal() == False and counter < 10):
-    cellType, newPos, packagesLeft, isTerminal = FR.takeAction(
-        chooseAction(FR))
-    print(FR.getPosition())
-    print(cellType)
-    counter += 1
-FR.showPath(0)
+
+initialState = FR.getPosition()
+print("Agent initial state", initialState)
+for episodes in range(episodes):
+    FR.newEpoch()
+
+    print("episode", episodes)
+    counter = 0
+    while(FR.isTerminal() == False and counter < 10000):
+       # print("QQQQQQ", Q_values[FR.getPosition(), :])
+        numMoves += 1
+        counter += 1
+
+        # choose an action
+
+        actionNum = chooseAction(FR)
+        # print("we are in state", FR.getPosition())
+        # print("we are moving:", ac[actionNum])
+        oldState = FR.getPosition()  # the old state before our action
+        cellType, state, packagesLeft, isTerminal = FR.takeAction(
+            actions[actionNum])  # if the action takes me into a non traversable space the same position is returned
+
+        if(cellType == 1 or cellType == 2 or cellType == 3):  # the cell type is a package
+            reward = 100
+        elif(oldState == state):
+            reward = -100
+        else:
+            reward = -1  # rewards[state]
+
+        # print("moved to state", state, "with reward", reward)
+
+        # update Q values
+
+        Q_values[convert(oldState), actionNum] = Q_values[convert(oldState), actionNum] + learning_rate * \
+            (reward + discount_factor *
+             np.max(Q_values[convert(state), :])-Q_values[convert(oldState), actionNum])
+    print(numMoves)
+    numMoves = 0
+
+
+# print(Q_values)
+FR.showPath(-1)
